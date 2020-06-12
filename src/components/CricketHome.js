5838ceec157b50ec5d2f7c6f1d909567e8e91b68
sudama6998 <sudamajaiswal6998@gmail.com>
2020-06-12 19:44:08 +0530

    Final Code

--- src/components/CricketHome.js
+++ src/components/CricketHome.js
@@ -1,6 +1,16 @@
+// Rules for the Game of Cricket
+// Match is of 10 overs
+// If wickets is equal to 10 or over is equal to 10 the match will stopped
+// It will generate random number between 0 and 8
+// If number is equal to 8 then it is wickets
+// If number is equal to 7 then it can be noball or wide ball
+// If number is 1 or 3 then stricker batsman will swap with non stricker
+// Remaining number will only update the total score - Sudama [12/06/2020]
+
 import React, { Component } from 'react';
 import '../assests/styles/CricketHome.css';
 
+// Globally declared variable to set interval for every 1 second - Sudama [12/06/2020]
 let matchInterval;
 
 class CricketHome extends Component {
@@ -16,25 +26,31 @@ class CricketHome extends Component {
     btnDisabled : false,
   }
 
+  // Method to Stop the Interval to Stop the Running Match - Sudama [12/06/2020]
   stopMatch = () => {
     clearInterval(matchInterval);
   }
 
+  // Method if there is a wicket - Sudama [12/06/2020]
   itsWicket = () => {
     let totalWickets = this.state.wickets + 1;
     let newCurrentStricker = this.state.stricker;
+    // If Wicket is equal or more than 10 then stop the match - Sudama [12/06/2020]
     if(totalWickets >= 10) {
       this.stopMatch();
     } else {
+      // Else Change the Batsman with the new Batsman - Sudama [12/06/2020]
       newCurrentStricker = `Batsman${totalWickets + 2}`
     }
 
+    // Updating value in state - Sudama [12/06/2020]
     this.setState({
       stricker: newCurrentStricker,
       wickets : totalWickets
     })
   }
 
+  // Method if there is extra run if random number is 7 - Sudama [12/06/2020]
   itsExtra = () => {
     this.setState({
       extras: this.state.extras + 1,
@@ -42,7 +58,9 @@ class CricketHome extends Component {
     })
   }
 
+  // Method if there is an odd score - Sudama [12/06/2020]
   itsOddScore = (ballScore) => {
+    // Code to Swap the Stricker with Non-Stricker - Sudama [12/06/2020]
     let currentStricker = this.state.nonStricker;
     let currentNonStricker = this.state.stricker;
 
@@ -53,6 +71,7 @@ class CricketHome extends Component {
     })
   }
 
+  // All Other Remaining Condition - Sudama [12/06/2020]
   otherScore = (ballScore) => {
     this.setState({
       totalScore: this.state.totalScore + ballScore
@@ -60,16 +79,18 @@ class CricketHome extends Component {
   }
 
 
-
+  // Method to Generate Score and Call the Methods according to the condition - Sudama [12/06/2020]
   generateScore = () => {
     const currentScore = Math.floor(Math.random() * (8 - 0 + 1)) + 0;
     
     let updatedBallCount = this.state.ballCount;
     let updatedOverCount = this.state.overCount;
 
+    // If Over is equal or more than 10 then stop the match - Sudama [12/06/2020]
     if(this.state.overCount >= 10) {
       this.stopMatch();
-    } else {
+    } else { // If Not then update the ball count - Sudama [12/06/2020]
+      // If ball count is equal to six then update over and reset ball count - Sudama [12/06/2020]
       if(updatedBallCount >= 5 ) {
         updatedBallCount = 0;
         updatedOverCount++;
@@ -77,6 +98,7 @@ class CricketHome extends Component {
         updatedBallCount++;
       }
 
+      // Call Function according to generated Score - Sudama [12/06/2020]
       if(currentScore === 8) {
         this.itsWicket();
       } else if(currentScore === 7) {
@@ -95,6 +117,7 @@ class CricketHome extends Component {
     })
   }
 
+  // Method to Start the Match
   startMatch = () => {
     this.setState({
       btnDisabled : true
@@ -104,7 +127,7 @@ class CricketHome extends Component {
 
   render() {
     return (
-
+      // Code to Display Score Board to user - Sudama [12/06/2020]
       <div className="container">
         <div className="cricket_card py-2">
           <div className="container-fluid">
@@ -119,6 +142,7 @@ class CricketHome extends Component {
             <p className="other_details mb-4">Non-Striker : {this.state.nonStricker}</p>
 
             <div className="row w-100 m-0">
+              {/* Button to Start Match - Sudama [12/06/2020] */}
               <button className="btn btn-primary m-auto start_match_btn" onClick={() => this.startMatch()} disabled={this.state.btnDisabled}>Start Match</button>
             </div>
           </div>
